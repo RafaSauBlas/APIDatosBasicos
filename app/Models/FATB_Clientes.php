5139df894d310144df4767e73d33ab703fe229f5
Rafael Saucedo Blas <RafaSauBlas@hotmail.com>
2022-04-11 10:57:40 -0500

    Se quita el 'Has factory' en los modelos de las tablas

--- app/Models/FATB_Clientes.php
+++ app/Models/FATB_Clientes.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FATB_Clientes extends Model
@@ -12,5 +11,4 @@ class FATB_Clientes extends Model
     protected $fillable = array('FAnv_Razon', 'FAnv_Cd', 'FAnv_FiscalCd', 'FAnv_FiscalColonia', 'FAnv_ApartadoPost', 'FAnv_DirFiscal',
                                 'FAnv_Calle', 'FAnv_Cel', 'FAnv_APaterno', 'FAnv_AMaterno', 'FAnv_Nombres');
     public $timestamps = false;
-    use HasFactory;
 }
