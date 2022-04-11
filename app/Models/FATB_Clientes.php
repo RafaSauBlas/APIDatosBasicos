16d12383f01b16480944828dfaf6c9f15ad38802
Rafael Saucedo Blas <RafaSauBlas@hotmail.com>
2022-04-11 10:55:30 -0500

    En los modelos se agregan los campos editables y las llaves primarias de cada una de las tablas

--- app/Models/FATB_Clientes.php
+++ app/Models/FATB_Clientes.php
@@ -8,9 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class FATB_Clientes extends Model
 {
     public $table = 'FATB_Clientes';
-    protected $fillable = array('FAnv_FiscalColonia', 'FAin_FiscalCP', 'FAnv_Calle', 'FAnv_Tel', 'FAnv_Cel', 'FAnv_RFC',
-                                'FAnv_CURP', 'FAnv_Email', 'FAnv_Municipio', 'FAnv_Estado', 'FAnv_APaterno', 'FAnv_AMaterno', 'FAnv_Razon',
-                                'FAnv_Sexo', 'FAnv_IFE');
+    protected $primarykey = 'FAnv_CveCliente';
+    protected $fillable = array('FAnv_Razon', 'FAnv_Cd', 'FAnv_FiscalCd', 'FAnv_FiscalColonia', 'FAnv_ApartadoPost', 'FAnv_DirFiscal',
+                                'FAnv_Calle', 'FAnv_Cel', 'FAnv_APaterno', 'FAnv_AMaterno', 'FAnv_Nombres');
     public $timestamps = false;
     use HasFactory;
 }
--- app/Models/FATB_DistibuidorVales.php
+++ app/Models/FATB_DistibuidorVales.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class FATB_DistibuidorVales extends Model
 {
     public $table = 'FATB_DistibuidorVales';
+    protected $primarykey = 'FAdc_IdVale';
     public $timestamps = false;
 }
